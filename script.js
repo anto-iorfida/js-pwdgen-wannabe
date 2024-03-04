a18a63a1947ec9b3fd0ac1f1a6aef7d9abafa4d6
anto-iorfida <antonioiorfida@hotmail.com>
2024-03-04 18:42:37 +0100

    Add Bonus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Chiedi all’utente il suo nome,poi chiedi il suo cognome,poi chiedi il suo colore preferito.Infine scrivi sulla pagina nomecognomecolorepreferito23
+// Chiedi all’utente il suo nome,poi chiedi il suo cognome,poi chiedi il suo colore preferito.Infine scrivi sulla pagina nomecognomecolorepreferito23 BONUS(chiedi un numero e sommalo al 23)
 
 // chiedi nome
 const userName = prompt('Dimmi il tuo nome');
@@ -12,8 +12,20 @@ console.log(userSurname);
 const userColor = prompt('qual è il tuo colore preferito?')
 console.log(userColor);
 
+//chiedi numero preferito
+const userNumber =prompt('qual è il tuo numero preferito?')
+console.log(userNumber)
+
+// converti il numero cosi da sommarlo
+const userNumberAsNumber = parseInt(userNumber)
+console.log(userNumberAsNumber)
+
+// sommare numero scento a 23
+const sum = userNumberAsNumber + 23;
+console.log(sum)
+
 // genera passsword
-const userMessage = `${userName}${userSurname}${userColor}23`
+const userMessage = `${userName}${userSurname}${userColor}${sum}`;
 console.log(userMessage);
 
 // visualizzazione messaggio su html 
